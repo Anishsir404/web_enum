ccc26e906d5cf69b031945d6bbf5806e2c75564b
anish bhattarai <anishji0306@gmail.com>
2023-06-11 18:51:57 +0545

    adding fuzzer

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import requests
 import builtwith
-import subprocess
+
 # Input URL
 # url = input("Enter the target URL: ")
-url = "https://pwn.college/dojos"
+url = "https://softwarica.edu.np"
 
 # Technology Detection
 def detect_technologies(url):
     technologies = builtwith.builtwith(url)
-    return technologies
+    
+    print("Technologies used:")
+    for category, tech in technologies.items():
+        print(f"{category}:")
+        for t in tech:
+            print(t)
+            # Step 2: Search for Exploits
+            # search_exploits(t)
 
-technologies_used = detect_technologies(url)
-print("Technologies used:")
-for category, tech in technologies_used.items():
-    print(f"{category}:")
-    for t in tech:
-        print(t)
+# Search for Exploits
+def search_exploits(technology):
+    query = f"{technology} exploit"
+    url = f"https://www.exploit-db.com/search?description={query}"
+    response = requests.get(url)
+    # Process the response and extract relevant information
+    # (e.g., exploit titles, URLs, descriptions, etc.)
+    # Display the results
+def fuzz_directories(url):
+    with open("files/common.txt", "r") as file:
+        common_directories = file.read().splitlines()
+
+    for directory in common_directories:
+        directory_url = url + "/" + directory
+        response = requests.get(directory_url)
+        if response.status_code != 404 and response.status_code != 403:
+            print(f"[{response.status_code}] -- {directory_url}")
+
+
+
+# Step 1: Detect Technologies
+# detect_technologies(url)
+fuzz_directories(url)
